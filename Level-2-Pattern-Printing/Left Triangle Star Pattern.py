2cbc17b9ce0b207051bf969ed82715a9ee5493dd
vickii20 <vigneshramamoorthy04@gmail.com>
2025-07-30 04:38:36 +0530

    initial commit

--- Level-2-Pattern-Printing/Left Triangle Star Pattern.py
+++ Level-2-Pattern-Printing/Left Triangle Star Pattern.py
@@ -1,21 +1,55 @@
 value = int(input("Enter the No.of Lines: "))
 
-def patternA(n):
-  for row in range(1,n+1):
-    for col in range(1,row+1):
-      print('*',end='')
-    print()
+def patternA(n):                               # *
+  for row in range(1,n+1):                     # * *
+    for col in range(1,row+1):                 # * * *
+      print('*',end=' ')                        # * * * *
+    print()                                    # * * * * * 
 
 patternA(value)
 
 print('\n')
 
-def patternB(n):
-  for row in range(1,n+1):
-    for col in range(1,row+1):
-      print(col,end='')
-    print()
+def patternB(n):                               # 1
+  for row in range(1,n+1):                     # 1 2
+    for col in range(1,row+1):                 # 1 2 3
+      print(col,end=' ')                        # 1 2 3 4 
+    print()                                    # 1 2 3 4 5
 
 patternB(value)
+
+print('\n')
+
+def patternC(n):                               # 1
+  initial_val = 1                              # 0 1
+                                               # 1 0 1
+  for row in range(1,n+1):                     # 0 1 0 1
+                                               # 1 0 1 0 1
+    if row % 2 == 0:
+      initial_val = 0
+    else:
+      initial_val = 1
+
+    for col in range(1,row+1):
+
+      print(initial_val,end=' ')
+      initial_val = int(not initial_val)
+    print()
+
+patternC(value)
+
+print('\n')
+
+def patternD(n):
+
+  initial_val = 1
+
+  for row in range(1,n+1):
+    for col in range(1,row+1):
+      print(initial_val,end=' ')
+      initial_val += 1
+    print()
+
+patternD(value)
 
 print('\n')
