8152949f8e522e16b787bc01d9892d13751ea400
vickii20 <vigneshramamoorthy04@gmail.com>
2025-07-30 04:40:19 +0530

    initial commit

--- Level-2-Pattern-Printing/Left Triangle Star Pattern.py
+++ Level-2-Pattern-Printing/Left Triangle Star Pattern.py
@@ -3,7 +3,7 @@ value = int(input("Enter the No.of Lines: "))
 def patternA(n):                               # *
   for row in range(1,n+1):                     # * *
     for col in range(1,row+1):                 # * * *
-      print('*',end=' ')                        # * * * *
+      print('*',end=' ')                       # * * * *
     print()                                    # * * * * * 
 
 patternA(value)
@@ -13,7 +13,7 @@ print('\n')
 def patternB(n):                               # 1
   for row in range(1,n+1):                     # 1 2
     for col in range(1,row+1):                 # 1 2 3
-      print(col,end=' ')                        # 1 2 3 4 
+      print(col,end=' ')                       # 1 2 3 4 
     print()                                    # 1 2 3 4 5
 
 patternB(value)
@@ -40,11 +40,11 @@ patternC(value)
 
 print('\n')
 
-def patternD(n):
-
-  initial_val = 1
-
-  for row in range(1,n+1):
+def patternD(n):                               # 1
+                                               # 2 3
+  initial_val = 1                              # 4 5 6
+                                               # 7 8 9 10
+  for row in range(1,n+1):                     # 11 12 13 14 15 
     for col in range(1,row+1):
       print(initial_val,end=' ')
       initial_val += 1
